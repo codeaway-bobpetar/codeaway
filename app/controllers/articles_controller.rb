1abcf41895dc077197965be1c30ca1c1fc9e2537
Petar Risteski <bobpetar@hotmail.com>
2016-03-01 23:37:02 +0000

    design of portfolio page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,16 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
+  def index
+    @articles = Article.all
+  end
   
   def new
     @article = Article.new
   end
   
   def edit
-    @article = Article.find(params[:id])
-    
+
   end
   
   def create
@@ -20,7 +24,6 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfuly updated"
       redirect_to article_path(@article)
@@ -30,7 +33,13 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+
+  end
+  
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was successfuly destroyed"
+    redirect_to articles_path
   end
   
 
@@ -38,6 +47,9 @@ class ArticlesController < ApplicationController
   
   
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title,:description)
     end

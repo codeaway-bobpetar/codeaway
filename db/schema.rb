e5e7c1d92785e4e8f29c42ebb6f6b8863fa76584
Petar Risteski <bobpetar@hotmail.com>
2016-03-02 23:43:39 +0000

    password things

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302224639) do
+ActiveRecord::Schema.define(version: 20160302233643) do
 
   create_table "articles", force: :cascade do |t|
     t.string  "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160302224639) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
